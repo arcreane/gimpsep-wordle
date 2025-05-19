7e79a392cd32e92f9364f659506356e5b4317ee4
CassandreLam <153280933+CassandreLam@users.noreply.github.com>
2025-05-19 13:02:38 +0200

    comments on panorama

--- gimpw/gimpw/panorama.cpp
+++ gimpw/gimpw/panorama.cpp
@@ -3,27 +3,29 @@
 #include <QDebug>
 
 ImageStitcher::ImageStitcher() {
-
+    //create the OpenCV stitcher in panorama mode
     stitcher = cv::Stitcher::create(cv::Stitcher::PANORAMA);
 }
 
 bool ImageStitcher::stitchImages(const QStringList& filePaths, cv::Mat& output) {
     std::vector<cv::Mat> images;
 
+    //load each image from the Qt file list
     for (const QString& path : filePaths) {
         cv::Mat img = cv::imread(path.toStdString());
         if (img.empty()) {
             qWarning() << "failed to load image:" << path;
-            return false;
+            return false; //if any image fails we stop
         }
         images.push_back(img);
     }
 
+    //try to stitch all the images into a panorama
     cv::Stitcher::Status status = stitcher->stitch(images, output);
     if (status != cv::Stitcher::OK) {
         qWarning() << "stitching failed with status code:" << static_cast<int>(status);
-        return false;
+        return false; //return false if stitching didn't work
     }
 
-    return true;
+    return true; // success
 }
--- gimpw/gimpw/panorama.h
+++ gimpw/gimpw/panorama.h
@@ -5,13 +5,19 @@
 #include <opencv2/stitching.hpp>
 #include <QStringList>
 
+//this class handles panorama stitching using OpenCV's Stitcher module
 class ImageStitcher {
 public:
+    //constructor initializes the OpenCV stitcher
     ImageStitcher();
+
+    //stitches a list of image paths from qt into a single panorama
+    //returns true if successful or false otherwise
     bool stitchImages(const QStringList& filePaths, cv::Mat& output);
 
 private:
+    //the OpenCV stitcher instance
     cv::Ptr<cv::Stitcher> stitcher;
 };
 
-#endif 
+#endif *
