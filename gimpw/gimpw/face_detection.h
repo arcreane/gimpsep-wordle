13fed5241bf8e85fbf3a4b1e583fbefcd193e088
CassandreLam <153280933+CassandreLam@users.noreply.github.com>
2025-05-26 14:14:05 +0200

    Update face_detection.h
    Ajout de classes

--- gimpw/gimpw/face_detection.h
+++ gimpw/gimpw/face_detection.h
@@ -3,11 +3,23 @@
 
 #include <opencv2/opencv.hpp>
 
-void detectFacesCats(cv::Mat& image,
-    cv::CascadeClassifier& face_cascade,
-    cv::CascadeClassifier& smile_cascade,
-    cv::CascadeClassifier& cat_cascade);
+class Detector {
+private:
+    cv::CascadeClassifier face_cascade;
+    cv::CascadeClassifier smile_cascade;
+    cv::CascadeClassifier cat_cascade;
 
-cv::Mat applyDetection(const cv::Mat& input);
+public:
+    // charge les fichiers Haar cascade
+    bool loadClassifiers(const std::string& face_xml,
+        const std::string& smile_xml,
+        const std::string& cat_xml);
+
+    // détection pour sourires, visages et chats
+    void detectFacesCats(cv::Mat& image);
+
+    // appliquer la détection complète sur une image
+    cv::Mat applyDetection(const cv::Mat& input);
+};
 
 #endif
