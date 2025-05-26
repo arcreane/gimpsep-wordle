6ca0cdb926071c42d5677198a5fe210c771eaec2
CassandreLam <153280933+CassandreLam@users.noreply.github.com>
2025-05-26 14:15:17 +0200

    Update test_module_face.cpp

--- gimpw/gimpw/test_module_face.cpp
+++ gimpw/gimpw/test_module_face.cpp
@@ -1,28 +1,25 @@
-﻿#include "detection_and_morphology.h"
-#include "image_morphology.h"
+﻿#include "image_morphology.h"
 #include "face_detection.h"
 
 using namespace cv;
 using namespace std;
 
+// apply complet detection (faces, smiles, cats)
 cv::Mat applyDetection(const cv::Mat& input) {
     string face_xml = "data/haarcascades/haarcascade_frontalface_alt.xml";
     string smile_xml = "data/haarcascades/haarcascade_smile.xml";
     string cat_xml = "data/haarcascades/haarcascade_frontalcatface.xml";
 
-    CascadeClassifier face_cascade, smile_cascade, cat_cascade;
-    if (!face_cascade.load(face_xml) ||
-        !smile_cascade.load(smile_xml) ||
-        !cat_cascade.load(cat_xml)) {
+    Detector detector;
+    if (!detector.loadClassifiers(face_xml, smile_xml, cat_xml)) {
         cerr << "Error loading Haar cascade files." << endl;
         return input.clone();
     }
 
-    Mat image = input.clone();
-    detectFacesCats(image, face_cascade, smile_cascade, cat_cascade);
-    return image;
+    return detector.applyDetection(input); // detect and return the modified image
 }
 
+// apply morphology from GUI 
 cv::Mat applyMorphologyGUI(const cv::Mat& input, int size, bool dilate) {
-    return applyMorphology(input, size, dilate);
+    return Morphology::applyMorphology(input, size, dilate);
 }
